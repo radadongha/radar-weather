663da4eda4ad359bce95987c1df57da88e7e0437
radadongha <radadongha2010@gmail.com>
2025-07-20 06:41:56 +0700

    Cập nhật dong-ha-mon250719233004.MAX2332.jpg

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
-﻿Set WshShell = CreateObject("WScript.Shell") 
-WshShell.Run chr(34) & "C:\Users\kttv\Desktop\radar-weather\run_update.bat" & Chr(34), 0
-Set WshShell = Nothing
+﻿@echo off
+cd /d "C:\Users\kttv\Desktop\radar-weather"
+python update_github.py
+pause
