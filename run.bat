7cbfd1d9256f92c0879ac129f3889d724fbf8eec
radadongha <radadongha2010@gmail.com>
2025-07-19 17:09:07 +0700

    Cập nhật ảnh radar: dong-ha-mon250719100004.MAX2251.jpg

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
-﻿python update_github.py
+﻿Set WshShell = CreateObject("WScript.Shell") 
+WshShell.Run chr(34) & "C:\Users\kttv\Desktop\radar-weather\run_update.bat" & Chr(34), 0
+Set WshShell = Nothing
