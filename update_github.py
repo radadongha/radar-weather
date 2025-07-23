1c6d73dbda87859376681f04312b754638606926
radadongha <radadongha2010@gmail.com>
2025-07-23 13:50:41 +0700

    🛰️ Cập nhật ảnh radar tự động

--- update_github.py
+++ update_github.py
@@ -76,7 +76,7 @@ def generate_html(image_paths):
             gap: 10px;
         }}
         button {{
-            font-size: 8px;
+            font-size: 4px;
             padding: 10px 16px;
             cursor: pointer;
             border: none;
